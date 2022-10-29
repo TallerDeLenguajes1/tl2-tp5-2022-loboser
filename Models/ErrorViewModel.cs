062e99b67c0264503b7178fb2ae4e169d1803600
Zair <loboser.sl@gmail.com>
2022-10-29 10:25:59 -0300

    creacion del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_tp5_2022_loboser.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
